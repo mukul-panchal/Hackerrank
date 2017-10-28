9ebb817bdb331c0444abae3fe19db157b4c85622
mukul-panchal <33025965+mukul-panchal@users.noreply.github.com>
2017-10-28 15:53:56 +0530

    Create Solution.java

---
+++ 30 Days of Code Challenges/Day 5- Loops/Solution.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+                        
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+  for(int x=1;x<=10;x++){
+      System.out.printf("%d x %d = %d\n",n,x,n*x);
+  }
+    
+    }
+}
