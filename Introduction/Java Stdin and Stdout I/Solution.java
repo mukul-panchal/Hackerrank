8bcb1de8d8b4b6074b42d446f8e52852f0bcfaec
mukul-panchal <33025965+mukul-panchal@users.noreply.github.com>
2017-10-28 11:43:48 +0530

    Update Solution.java

--- Introduction/Java Stdin and Stdout I/Solution.java
+++ Introduction/Java Stdin and Stdout I/Solution.java
@@ -1,33 +1,18 @@
-    import java.io.*;
-    import java.util.*;
-    import java.text.*;
-    import java.math.*;
-    import java.util.regex.*;
+import java.util.*;
 
-    public class Solution {
+public class Solution {
 
-        public static void main(String[] args) {
-
-            Scanner sc=new Scanner(System.in);
-            int n=sc.nextInt();            
-            String ans="";
-            if(n%2==1){
-              ans = "Weird";
-             
-            }
-            else{
-            if(n>2&n<5){
-                 ans = "Not Weird";
-            }else if(n>6&n<=20){
-                              ans = "Weird";
-            }else{
-                                
-                ans = "Not Weird";
-
-            }
-                           
-            }
-            System.out.println(ans);
-            
-        }
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+        scan.close();
+      
+        System.out.println(a);
+                System.out.println(b);
+                System.out.println(c);
+        // Complete this line
+        // Complete this line
     }
+}
