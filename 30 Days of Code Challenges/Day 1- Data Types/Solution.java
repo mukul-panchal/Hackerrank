07972bc91852b269c4f0576f713086a3bb958445
mukul-panchal <33025965+mukul-panchal@users.noreply.github.com>
2017-10-28 15:46:39 +0530

    Create Solution.java

---
+++ 30 Days of Code Challenges/Day 1- Data Types/Solution.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+	
+    public static void main(String[] args) {
+        int i = 4;
+        double d = 4.0;
+        String s = "HackerRank ";
+		
+        Scanner scan = new Scanner(System.in);
+                /* Declare second integer, double, and String variables. */
+ /* Declare second integer, double, and String variables. */
+          int j=scan.nextInt();
+          double e=scan.nextDouble();
+           scan.nextLine();
+          String str=scan.nextLine();
+        /* Read and save an integer, double, and String to your variables.*/
+        // Note: If you have trouble reading the entire String, please go back and review the Tutorial closely.
+
+        /* Print the sum of both integer variables on a new line. */
+        System.out.println(i+j);
+        System.out.println(d+e);
+        System.out.println(s+str);
+      
+        
+        
+        
+        
+                scan.close();
+    }
+}
