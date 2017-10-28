8f273ed511cf0cfc1c0383692f3d7ca4d0a52845
mukul-panchal <33025965+mukul-panchal@users.noreply.github.com>
2017-10-28 15:58:17 +0530

    Update Solution.java

--- 30 Days of Code Challenges/Day 0- Hello- World./Solution.java
+++ 30 Days of Code Challenges/Day 0- Hello- World./Solution.java
@@ -4,6 +4,7 @@ import java.text.*;
 import java.math.*;
 import java.util.regex.*;
 
+
 public class Solution {
 	public static void main(String[] args) {
         // Create a Scanner object to read input from stdin.
@@ -17,7 +18,8 @@ public class Solution {
 		scan.close(); 
       
 		// Print a string literal saying "Hello, World." to stdout.
-		System.out.println("Hello, World.");
+		System.out.println("Hello, World.\n" +inputString);
+        
       
 	    // TODO: Write a line of code here that prints the contents of inputString to stdout.
 	}
